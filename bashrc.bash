aaf9ee566e1a021be79f93f6ab6c3f39ce9d94fb
JP Addison <johnpaddison@gmail.com>
2018-01-08 10:42:10 -0800

    more progress on ideavim, add perl path stuff from pg_prove install

--- bashrc.bash
+++ bashrc.bash
@@ -48,3 +48,9 @@ export NVM_DIR="$HOME/.nvm"
 
 # Homebrew Coreutils
 alias timeout=gtimeout
+
+PATH="/Users/jpaddison/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/Users/jpaddison/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/Users/jpaddison/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/Users/jpaddison/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/Users/jpaddison/perl5"; export PERL_MM_OPT;
